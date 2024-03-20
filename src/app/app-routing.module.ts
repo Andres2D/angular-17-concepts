c48150f9fa8a48e89901c42e6b05265c5ea81ce6
Andres2D <andres2d1997.1206@gmail.com>
2024-03-20 18:51:51 -0400

    Implement lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './screens/home-page/home-page.component';
-import { RegisterPageComponent } from './screens/register-page/register-page.component';
-import { ErrorPageComponent } from './screens/error-page/error-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent,
+    loadChildren: () => import('./screens/home-page/home-page.module').then(m => m.HomePageModule)
   },
   {
     path: 'register',
-    component: RegisterPageComponent
+    loadChildren: () => import('./screens/register-page/register-page.module').then(m => m.RegisterPageModule)
   },
   {
     path: '404',
-    component: ErrorPageComponent
+    loadChildren: () => import('./screens/error-page/error-page.module').then(m => m.ErrorPageModule)
   },
   {
     path: '**',
